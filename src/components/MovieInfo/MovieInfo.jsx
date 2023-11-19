476ca27e9256878450ead0a00df5bfa2cdffd914
Abhinav Moghe <abhinavmoghe01@gmail.com>
2023-11-19 22:54:15 +0530

    Basic styling for movie info

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -3,7 +3,8 @@ import './styles.css'
 const MovieInfo = () => {
   return (
     <div className='movie-info'>
-      
+      <div className='info-box'>Trailer will be displayed here</div>
+      <div className='info-box'>Information will be displayed here</div>
     </div>
   )
 }
