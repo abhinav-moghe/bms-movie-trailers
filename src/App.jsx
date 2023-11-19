f3043bfbf544b935351631b9623990c70ade03e4
Abhinav Moghe <abhinavmoghe01@gmail.com>
2023-11-19 12:34:44 +0530

    Added loader, fetching data and passing it using useContext()

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 import './App.css'
 
+import { createContext, useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 
-const App = () => {
-  return (
-    <div className="container-main">
-      <Navbar />
+const DataContext = createContext()
 
-      <div className="wrapper-content">
-        {/* movies, trailer and info */}
-      </div>
-    </div>
+const App = () => {
+  // TO-DO: remove this
+  console.clear()
+
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({
+    languageList: [],
+    moviesData: [],
+  })
+
+  // TO-DO: remove this
+  console.log(data)
+
+  useEffect(() => {
+    setTimeout(() => {
+      // GET languageList and moviesData
+      fetch('https://in.bmscdn.com/m6/static/interview-mock/data.json', { method: 'GET' })
+        .then(response => response.json())
+        .then(d => {
+          // create an array for language list to handle
+          setData({
+            languageList: d.languageList.map(o => { return { text: o, isSelected: false } }),
+            moviesData: d.moviesData
+          });
+          setLoading(false)
+        })
+    }, 2000)
+  }, [])
+
+  return (
+    <>
+      {
+        loading ?
+          <p>Loading . . .</p>
+          :
+          <DataContext.Provider value={data}>
+            <div className="container-main">
+              <Navbar />
+
+              <div className="wrapper-content">
+                {/* movies, trailer and info */}
+              </div>
+            </div>
+          </DataContext.Provider>
+      }
+    </>
   )
 }
 
